03f0f223c4a16633aad250ebae9ff5a9d6072e08
Will Carpenter <willcarpenteriii@gmail.com>
2017-11-22 21:40:01 -0700

    added axios, created plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  
+    <div class="f1 code">
+        Hello, wwww
+        <ul>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+        </ul>
+        {{ users }}
+    </div>
 </template>
 
 <script>
-  
+    import axios from '../plugins/axios'
+
+    export default {
+    	asyncData() {
+    		return axios.get("users")
+                .then(res => ({
+                	users: res.data
+                }))
+        }
+    }
+
 </script>
 
 <style>
